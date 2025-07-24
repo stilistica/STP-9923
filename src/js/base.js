85685affa434a8e8c7439ac9b0faadea823234e7
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-24 18:46:52 +0300

    finished with base and burger-mneu

--- src/js/base.js
+++ src/js/base.js
@@ -2,7 +2,7 @@ const sections = document.querySelectorAll('[data-action="section-nothero"]');
 const observer = new IntersectionObserver((entries, obs) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
+      entry.target.setAttribute('data-visible', 'true');
       obs.unobserve(entry.target);
     }
   });
