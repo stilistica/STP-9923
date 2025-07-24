54d448a5cd07481cb01d8aea82a73072511bb0f4
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-07-24 18:50:11 +0300

    add cookies

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,19 +1,19 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-  if(!localStorage.getItem('cookiesAnswered')){
+window.addEventListener('DOMContentLoaded', () => {
+  if (!localStorage.getItem('cookiesAnswered')) {
     const cookies = document.querySelector('[data-action="cookies"]');
-    cookies.classList.add('active');
-    document.body.classList.add('no-scroll');
+    cookies.setAttribute('data-active', 'true');
+    document.body.setAttribute('data-scroll-locked', 'true');
   }
 });
 
-document.querySelector('[data-action="accept-cookies"]').addEventListener('click', ()=>{
+document.querySelector('[data-action="accept-cookies"]').addEventListener('click', () => {
   localStorage.setItem('cookiesAnswered', 'accepted');
-  document.querySelector('[data-action="cookies"]').classList.remove('active');
-  document.body.classList.remove('no-scroll');
+  document.querySelector('[data-action="cookies"]').setAttribute('data-active', 'false');
+  document.body.setAttribute('data-scroll-locked', 'false');
 });
 
-document.querySelector('[data-action="decline-cookies"]').addEventListener('click', ()=>{
+document.querySelector('[data-action="decline-cookies"]').addEventListener('click', () => {
   localStorage.setItem('cookiesAnswered', 'declined');
-  document.querySelector('[data-action="cookies"]').classList.remove('active');
-  document.body.classList.remove('no-scroll');
+  document.querySelector('[data-action="cookies"]').setAttribute('data-active', 'false');
+  document.body.setAttribute('data-scroll-locked', 'false');
 });
